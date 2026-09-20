b0e92ac933d37aa9f33531d41191bc8a8824a263
Diwakar Kapil <007.diwakarkapil@gmail.com>
2026-09-20 16:08:54 +0530

    Solved: Segregate 0s and 1s (geeksforgeeks)

--- geeksforgeeks/Practice/segregate-0s-and-1s5106.cpp
+++ geeksforgeeks/Practice/segregate-0s-and-1s5106.cpp
@@ -3,22 +3,25 @@
 // Language: unknown
 // Verdict: Accepted
 // URL: https://www.geeksforgeeks.org/problems/segregate-0s-and-1s5106/1
-// Solved on: 2026-09-20T09:26:24.721Z
+// Solved on: 2026-09-20T10:38:53.195Z
 
-class Solution {
-  public:
-    void segregate0and1(vector<int> &arr) {
-        int n= size(arr);
-            int zero=0,one=0;
-            for(int i=0;i<n;i++){
-                if(arr[i]==0) zero++;
-                else one++;
-            }
-           for(int i=0;i<zero;i++){
-            arr[i]=0;
-           }
-           for(int i=zero;i<n;i++){
-            arr[i]=1;
-           }
-    }
+class Solution { 
+public: 
+    void segregate0and1(vector<int> &arr) { 
+        int i = 0, j = arr.size() - 1; 
+
+        while (i < j) { 
+            if (arr[i] == 0) { 
+                i++;
+            } 
+            else if (arr[j] == 1) { 
+                j--;
+            } 
+            else { 
+                swap(arr[i], arr[j]); 
+                i++; 
+                j--; 
+            } 
+        } 
+    } 
 };
