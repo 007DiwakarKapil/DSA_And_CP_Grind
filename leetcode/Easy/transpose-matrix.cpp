0e6e1bd42c1d0903bdd2ed3e25a6ea3b0f030012
Diwakar Kapil <007.diwakarkapil@gmail.com>
2026-09-24 19:26:07 +0530

    Solved: Transpose Matrix (leetcode)

---
+++ leetcode/Easy/transpose-matrix.cpp
@@ -0,0 +1,22 @@
+// Problem: Transpose Matrix
+// Platform: leetcode
+// Rating/Difficulty: Easy
+// Language: cpp
+// Verdict: Accepted
+// URL: https://leetcode.com/problems/transpose-matrix/
+// Solved on: 2026-09-24T13:56:06.513Z
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int r= matrix.size();
+        int c= matrix[0].size();
+       vector<vector<int>> result(c, vector<int>(r, 0));
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+               result[j][i]=matrix[i][j];
+            }
+        }
+        return result;
+    }
+};
